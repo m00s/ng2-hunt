336df4e79ebc9814a73d531bc6ba2da67bf35a58
Massimiliano Sartoretto <massimilianosartoretto@gmail.com>
2016-03-09 14:32:34 +0100

    Minor fixes in test config and helpers

--- helpers.js
+++ helpers.js
@@ -1,37 +1,41 @@
 var path = require('path');
 var zlib = require('zlib');
-var validateWebpackConfig = require('webpack-validator');
 
-// Helper functions
+var _root = path.resolve(__dirname, '..');
 
-exports.validateWebpackConfig = validateWebpackConfig;
-exports.validate = validateWebpackConfig;
+console.log('root directory:', root());
+
+function hasProcessFlag(flag) {
+  return process.argv.join('').indexOf(flag) > -1;
+}
 
 function gzipMaxLevel(buffer, callback) {
-  return zlib['gzip'](buffer, {level: 9}, callback)
+  return zlib['gzip'](buffer, {level: 9}, callback);
 }
-exports.gzipMaxLevel = gzipMaxLevel;
 
 function root(args) {
   args = Array.prototype.slice.call(arguments, 0);
-  return path.join.apply(path, [__dirname].concat(args));
+  return path.join.apply(path, [_root].concat(args));
 }
-exports.root = root;
 
 function rootNode(args) {
   args = Array.prototype.slice.call(arguments, 0);
   return root.apply(path, ['node_modules'].concat(args));
 }
-exports.rootNode = rootNode;
 
 function prependExt(extensions, args) {
   args = args || [];
   if (!Array.isArray(args)) { args = [args] }
   return extensions.reduce(function(memo, val) {
     return memo.concat(val, args.map(function(prefix) {
-      return prefix + val
+      return prefix + val;
     }));
   }, ['']);
 }
+
+exports.hasProcessFlag = hasProcessFlag;
+exports.gzipMaxLevel = gzipMaxLevel;
+exports.root = root;
+exports.rootNode = rootNode;
 exports.prependExt = prependExt;
 exports.prepend = prependExt;
