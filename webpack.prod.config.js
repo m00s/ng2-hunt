c12fb8f69f7d9755f74a0e4bf089175db975da3e
Massimiliano Sartoretto <massimilianosartoretto@gmail.com>
2016-03-13 19:32:34 +0100

    Update prod and test webpack config

--- webpack.prod.config.js
+++ webpack.prod.config.js
@@ -1,7 +1,10 @@
 
-var helpers = require('./helpers');
-// Webpack Plugins
+var helpers = require('./helpers'); // Helper: root(), and rootDir() are defined at the bottom
 var webpack = require('webpack');
+
+/**
+ * Webpack Plugins
+ */
 var ProvidePlugin = require('webpack/lib/ProvidePlugin');
 var DefinePlugin = require('webpack/lib/DefinePlugin');
 var OccurenceOrderPlugin = require('webpack/lib/optimize/OccurenceOrderPlugin');
@@ -11,12 +14,16 @@ var CommonsChunkPlugin = require('webpack/lib/optimize/CommonsChunkPlugin');
 var CompressionPlugin = require('compression-webpack-plugin');
 var CopyWebpackPlugin = require('copy-webpack-plugin');
 var HtmlWebpackPlugin = require('html-webpack-plugin');
-var WebpackMd5Hash    = require('webpack-md5-hash');
-var ENV = process.env.NODE_ENV = process.env.ENV = 'production';
-var HOST = process.env.HOST || 'localhost';
-var PORT = process.env.PORT || 8080;
+var WebpackMd5Hash = require('webpack-md5-hash');
+var ForkCheckerPlugin = require('awesome-typescript-loader').ForkCheckerPlugin;
 
-var metadata = {
+/**
+ * Webpack Constants
+ */
+const ENV = process.env.NODE_ENV = process.env.ENV = 'production';
+const HOST = process.env.HOST || 'localhost';
+const PORT = process.env.PORT || 8080;
+const METADATA = {
   title: 'Angular2 Webpack Starter by @gdi2990 from @AngularClass',
   baseUrl: '/',
   host: HOST,
@@ -24,169 +31,170 @@ var metadata = {
   ENV: ENV
 };
 
+module.exports = {
 
-helpers.validate({
-  // static data for index.html
-  metadata: metadata,
-
-  devtool: 'source-map',
-  cache: false,
+  metadata: METADATA,
   debug: false,
-
+  devtool: 'source-map',
   entry: {
-   'polyfills':'./src/polyfills.ts',
-   'main':'./src/main.ts' // our angular app
-  },
 
-  // Config for our build files
-  output: {
-   path: helpers.root('dist'),
-   filename: '[name].[chunkhash].bundle.js',
-   sourceMapFilename: '[name].[chunkhash].bundle.map',
-   chunkFilename: '[id].[chunkhash].chunk.js'
+    'polyfills': './src/polyfills.ts',
+    'vendor': './src/vendor.ts',
+    'main': './src/main.browser.ts'
+
   },
 
   resolve: {
-   cache: false,
-   // ensure loader extensions match
-   extensions: ['', '.ts','.js']
-  },
+    extensions: ['', '.ts', '.js'],
 
+    // Make sure root is src
+    root: helpers.root('src'),
+
+  },
+  output: {
+
+    path: helpers.root('dist'),
+    filename: '[name].[chunkhash].bundle.js',
+    sourceMapFilename: '[name].[chunkhash].bundle.map',
+    chunkFilename: '[id].[chunkhash].chunk.js'
+
+  },
   module: {
     preLoaders: [
-      {
-        test: /\.ts$/,
-        loader: 'tslint-loader',
-        exclude: [
-          helpers.root('node_modules')
-        ]
-      },
-      {
-        test: /\.js$/,
-        loader: 'source-map-loader',
-        exclude: [
-          helpers.root('node_modules/rxjs')
-        ]
-      }
+      {test: /\.ts$/, loader: 'tslint-loader', exclude: [helpers.root('node_modules')]},
+      {test: /\.js$/, loader: 'source-map-loader', exclude: [
+        // these packages have problems with their sourcemaps
+        helpers.root('node_modules/rxjs'),
+        helpers.root('node_modules/@angular2-material')
+      ]}
+
     ],
+
     loaders: [
-      // Support for .ts files.
       {
-        test: /\.ts$/,
-        loader: 'ts-loader',
+        test: /\.ts$/, loader: 'awesome-typescript-loader',
         query: {
-          // remove TypeScript helpers to be injected below by DefinePlugin
           'compilerOptions': {
-            'removeComments': true,
-            'noEmitHelpers': true,
+
+            'removeComments': true
+
           }
         },
         exclude: [
           /\.(spec|e2e)\.ts$/
         ]
       },
+      {test: /\.json$/, loader: 'json-loader'},
+      {test: /\.css$/, loader: 'raw-loader'},
+      {test: /\.html$/, loader: 'raw-loader', exclude: [helpers.root('src/index.html')]}
 
-      // Support for *.json files.
-      {
-        test: /\.json$/,
-        loader: 'json-loader'
-      },
+    ],
 
-      // Support for CSS as raw text
-      {
-        test: /\.css$/,
-        loader: 'raw-loader'
-      },
-
-      // support for .html as raw text
-      {
-        test: /\.html$/,
-        loader: 'raw-loader',
-        exclude: [
-          helpers.root('src/index.html')
-        ]
-      },
-
-      // Support for SCSS
-      { test: /\.scss$/, include: [path.resolve(__dirname, 'src/app/styles')], loader: 'style!css!!sass' }
+    noParse: [
+      helpers.root('zone.js', 'dist'),
+      helpers.root('angular2', 'bundles')
     ]
+
   },
 
   plugins: [
+
+    new ForkCheckerPlugin(),
+
     new WebpackMd5Hash(),
     new DedupePlugin(),
     new OccurenceOrderPlugin(true),
-    new CommonsChunkPlugin({
-      name: 'polyfills',
-      filename: 'polyfills.[chunkhash].bundle.js',
-      chunks: Infinity
-    }),
-    // static assets
-    new CopyWebpackPlugin([
-      {
-        from: 'src/assets',
-        to: 'assets'
-      }
-    ]),
-    // generating html
-    new HtmlWebpackPlugin({ template: 'src/index.html' }),
-    new DefinePlugin({
-      // Environment helpers
-      'process.env': {
-        'ENV': JSON.stringify(metadata.ENV),
-        'NODE_ENV': JSON.stringify(metadata.ENV)
-      }
-    }),
-    new ProvidePlugin({
-      // TypeScript helpers
-      '__metadata': 'ts-helper/metadata',
-      '__decorate': 'ts-helper/decorate',
-      '__awaiter': 'ts-helper/awaiter',
-      '__extends': 'ts-helper/extends',
-      '__param': 'ts-helper/param'
-    }),
+    new CommonsChunkPlugin({name: ['main', 'vendor', 'polyfills'], minChunks: Infinity}),
+    new CopyWebpackPlugin([{from: 'src/assets', to: 'assets'}]),
+    new HtmlWebpackPlugin({template: 'src/index.html', chunksSortMode: 'none'}),
+    new DefinePlugin({'ENV': JSON.stringify(METADATA.ENV), 'HMR': false}),
     new UglifyJsPlugin({
-      beautify: false,
-      mangle: false,
-      comments: false,
-      compress : {
-        screw_ie8 : true
-      },
-      // TODO(gdi2290): uncomment in beta.2
-      //mangle: {
-      //  screw_ie8 : true
-      //}
+      beautify: false,//prod
+      mangle: {
+        screw_ie8: true,
+        except: [
+          'App',
+          'About',
+          'Contact',
+          'Home',
+          'Menu',
+          'Footer',
+          'XLarge',
+          'RouterActive',
+          'RouterLink',
+          'RouterOutlet',
+          'NgFor',
+          'NgIf',
+          'NgClass',
+          'NgSwitch',
+          'NgStyle',
+          'NgSwitchDefault',
+          'NgControl',
+          'NgControlName',
+          'NgControlGroup',
+          'NgFormControl',
+          'NgModel',
+          'NgFormModel',
+          'NgForm',
+          'NgSelectOption',
+          'DefaultValueAccessor',
+          'NumberValueAccessor',
+          'CheckboxControlValueAccessor',
+          'SelectControlValueAccessor',
+          'RadioControlValueAccessor',
+          'NgControlStatus',
+          'RequiredValidator',
+          'MinLengthValidator',
+          'MaxLengthValidator',
+          'PatternValidator',
+          'AsyncPipe',
+          'DatePipe',
+          'JsonPipe',
+          'NumberPipe',
+          'DecimalPipe',
+          'PercentPipe',
+          'CurrencyPipe',
+          'LowerCasePipe',
+          'UpperCasePipe',
+          'SlicePipe',
+          'ReplacePipe',
+          'I18nPluralPipe',
+          'I18nSelectPipe'
+        ] // Needed for uglify RouterLink problem
+      }, // prod
+      compress: {screw_ie8: true}, //prod
+      comments: false //prod
     }),
-    // include uglify in production
+
     new CompressionPlugin({
-      algorithm: gzipMaxLevel,
+      algorithm: helpers.gzipMaxLevel,
       regExp: /\.css$|\.html$|\.js$|\.map$/,
       threshold: 2 * 1024
     })
+
   ],
-  // Other module loader config
+
   tslint: {
     emitErrors: true,
     failOnHint: true,
-    resourcePath: 'src',
+    resourcePath: 'src'
   },
 
   htmlLoader: {
     minimize: true,
     removeAttributeQuotes: false,
     caseSensitive: true,
-    customAttrSurround: [ [/#/, /(?:)/], [/\*/, /(?:)/], [/\[?\(?/, /(?:)/] ],
-    customAttrAssign: [ /\)?\]?=/ ]
+    customAttrSurround: [[/#/, /(?:)/], [/\*/, /(?:)/], [/\[?\(?/, /(?:)/]],
+    customAttrAssign: [/\)?\]?=/]
   },
-  // don't use devServer for production
 
-  // we need this due to problems with es6-shim
   node: {
     global: 'window',
-    progress: false,
+    process: false,
     crypto: 'empty',
     module: false,
     clearImmediate: false,
     setImmediate: false
   }
+
 };
